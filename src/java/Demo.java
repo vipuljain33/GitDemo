1019e15f9a6e862023d67ad24c83a8aecd5eea94
vipul jain <vipuljain33@gmail.com>
2019-07-18 14:33:16 +0530

    First ccommit

---
+++ src/java/Demo.java
@@ -0,0 +1,10 @@
+package java;
+
+public class Demo {
+
+
+    public void fun1()
+    {
+        System.out.println("inside fun1 in demo class");
+    }
+}
---
+++ src/java/Demo2.java
@@ -0,0 +1,9 @@
+package java;
+
+public class Demo2 {
+
+    public void fun2()
+    {
+        System.out.println("inside fun2 of demo2");
+    }
+}
